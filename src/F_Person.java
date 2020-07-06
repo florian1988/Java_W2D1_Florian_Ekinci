3203c288aab7c19cfa710ad3fe2df3a820dedf76
florian <flo.moerzinger@gmx.at>
2020-07-06 11:21:07 +0200

    Exercise 1 and 2

--- src/F_Person.java
+++ src/F_Person.java
@@ -1,8 +1,31 @@
 public class F_Person {
 
-    String name;
+    String firstname;
+    String lastname;
+
     int age;
     String job;
     String gender;
+
+    public void getFullName(){
+
+        if (gender == "m") {
+            System.out.println("His full name is " + firstname + " " + lastname + ".");
+            System.out.println("His age is" + age + " and his job is" + job + ".");
+        } else {
+            System.out.println("Her full name is " + firstname + " " + lastname + ".");
+            System.out.println("Her age is" + age + " and her job is" + job + ".");
+        }
+    }
+
+    public void getFullNameTwo(){
+        if (gender == "m") {
+            System.out.println("His full name is " + firstname + " " + lastname.toUpperCase() + ".");
+            System.out.println("His age is" + age + " and his job is" + job + ".");
+        } else {
+            System.out.println("Her full name is " + firstname + " " + lastname.toUpperCase() + ".");
+            System.out.println("Her age is" + age + " and her job is" + job + ".");
+        }
+    }
 
 }
--- src/F_PersonOutput.java
+++ src/F_PersonOutput.java
@@ -1,24 +1,38 @@
 public class F_PersonOutput {
 
+    //Exercise A1 Create a class Person
+    //Exercise A2 (Intermediate)
+
+
     public static void main(String[] args) {
         F_Person p1 = new F_Person();
         F_Person p2 = new F_Person();
 
-        p1.name = "Frank";
+        p1.firstname = "Frank";
+        p1.lastname = "Franklin";
         p1.age = 30;
         p1.job = "manager";
         p1.gender = "m";
 
-        p2.name = "Maria";
+        p2.firstname = "Maria";
+        p2.lastname = "Mangar";
         p2.age = 33;
         p2.job = "consultant";
         p2.gender = "f";
 
 
 
-        System.out.println( "The Person is " + p1.name + " his age is " + p1.age + " and he workes as a " + p1.job + ".");
-        System.out.print( "The Person is " + p2.name + " her age is " + p2.age + " and she workes as a " + p2.job + ".");
+        System.out.println( "The Person is " + p1.firstname + " his age is " + p1.age + " and he workes as a " + p1.job + ".");
+        System.out.println( "The Person is " + p2.firstname + " her age is " + p2.age + " and she workes as a " + p2.job + ".");
+        System.out.println(" ");
 
+        p1.getFullName();
+        p2.getFullName();
+
+        System.out.println(" ");
+
+        p1.getFullNameTwo();
+        p2.getFullNameTwo();
 
     }
 
